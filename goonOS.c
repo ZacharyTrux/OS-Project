919341bef8351547f304ad08f55bd8bc2b052bdd
ZacharyTrux <ztruxillo@gmail.com>
2025-05-13 17:14:45 -0500

    Co-authored-by: Kameron Stuckey <kam-stu@users.noreply.github.com> Co-authored-by: Nubbylord <Nubbylord@users.noreply.github.com>

--- goonOS.c
+++ goonOS.c
@@ -5,25 +5,81 @@
 
 #include <stdio.h>
 #include "virt-os.h"
+#include <stdlib.h>
 
-/*
-    functions to use :D 
-*/
-void callback(int num);
+#define MAX_SIZE 256
 
-void cmdExecutor();
+char string[MAX_SIZE];
+int ctr = 0;
 
+void callback(int key_code);
+void cmdExecutor(const char *command_string);
 
 int main(){
-    
 
-    printf("GOON MODE ACTIVATED: GOONERS UNITE\n");
-    printf("STARTING UP THE GOON DRIVERS\n");
-    printf("GOON ON BROTHER\n");
-    registerKeyPressCallback();
-   
+    registerKeyPressCallback(callback);
+    registerCommandHandler(cmdExecutor);
     initializeOS();
 
     return 0;
 }
+
+/*
+void callback(int keycode) {
+    if keycode is a valid printable character:
+        convert to char
+        insertKeyData(char as string)
+
+    else if keycode is ENTER:
+        insertKeyData("\n")
+        store current input as command
+        call command handler (implicitly triggers when Enter is pressed)
+
+    else if keycode is BACKSPACE:
+        insertKeyData("Backspace")
+
+    else if keycode is ESC:
+        exit(0)
+}
+*/
+
+void callback(int key_code){
+    // checks if key_code is a valid ASCII letter
+    printf("%d", key_code);
+    char character = (char)key_code;
+    printf("%c", character);
+    if (ctr < MAX_SIZE - 1) {
+        string[ctr++] = character;
+        string[ctr] = '\0';
+    }
+
+        char displayStr[2] = {character, '\0'};
+        insertKeyData(displayStr);
+    // checks if key_code is "enter" key
+    if(key_code == 13){
+        insertKeyData("\n");
+        cmdExecutor(string);
+        ctr = 0;
+        
+    }
+    // checks if key_code is "backspace" key
+    else if (key_code == 8){
+        insertKeyData("Backspace");
+        if (ctr > 0) {
+            string[ctr] = '\0';
+            ctr--;
+        }
+    }
+    // checks if key_code is "esc" key
+    else if (key_code == 27) {
+        exit(0);
+    } 
+
+}
+
+void cmdExecutor(const char *command_string){
+    
+}   
+
+
 
