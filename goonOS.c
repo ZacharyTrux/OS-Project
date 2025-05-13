b005d981ec7b62e1aae5dd8d977178af2546682b
ZacharyTrux <ztruxillo@gmail.com>
2025-05-13 17:52:36 -0500

    asdbasdkjda

--- goonOS.c
+++ goonOS.c
@@ -5,18 +5,23 @@
 
 #include <stdio.h>
 #include "virt-os.h"
+
 #include <stdlib.h>
 
 #define MAX_SIZE 256
 
 char string[MAX_SIZE];
 int ctr = 0;
+int keyMap[50] = {0}; 
 
+
+void initialize();
 void callback(int key_code);
 void cmdExecutor(const char *command_string);
 
 int main(){
 
+    initialize();
     registerKeyPressCallback(callback);
     registerCommandHandler(cmdExecutor);
     initializeOS();
@@ -45,16 +50,22 @@ void callback(int keycode) {
 
 void callback(int key_code){
     // checks if key_code is a valid ASCII letter
-    printf("%d", key_code);
-    char character = (char)key_code;
-    printf("%c", character);
+    
+    printf("%d\n", key_code);
+
+    /*
+    char character = keyMap[key_code];
     if (ctr < MAX_SIZE - 1) {
         string[ctr++] = character;
         string[ctr] = '\0';
     }
+    char displayStr[2] = {character, '\0'};
+    insertKeyData(displayStr);
+    */
 
-        char displayStr[2] = {character, '\0'};
-        insertKeyData(displayStr);
+    printf("Character: %c\n", keyMap[key_code]);
+    
+    /*
     // checks if key_code is "enter" key
     if(key_code == 13){
         insertKeyData("\n");
@@ -74,6 +85,7 @@ void callback(int key_code){
     else if (key_code == 27) {
         exit(0);
     } 
+        */
 
 }
 
@@ -83,3 +95,8 @@ void cmdExecutor(const char *command_string){
 
 
 
+void initialize(){
+    // initializes all characters...im dissapointed in you, you know why
+    keyMap[33] = 'a'; keyMap[12] = 'b';keyMap[11] = 'c';keyMap[4]  = 'd';keyMap[45] = 'e';keyMap[32] = 'f';keyMap[44] = 'g';keyMap[37] = 'h';keyMap[14] = 'i';keyMap[15] = 'j';keyMap[18] = 'k';keyMap[42] = 'm';keyMap[47] = 'l';keyMap[36] = 'n';keyMap[39] = 'o';keyMap[20] = 'p';keyMap[7]  = 'q';keyMap[2]  = 'r';keyMap[38] = 's';keyMap[9]  = 't';keyMap[8]  = 'u';keyMap[22] = 'v';keyMap[34] = 'w';keyMap[40] = 'x';keyMap[21] = 'y';keyMap[6]  = 'z';keyMap[46] = '1';keyMap[10] = '2';keyMap[5] = '3';keyMap[31] = '4';keyMap[25] = '5';keyMap[17] = '6';keyMap[28] = '7';keyMap[30] = '8';keyMap[23] = '9';keyMap[13] = '0';keyMap[29] ='[';keyMap[1] = ']';keyMap[48] = ';';keyMap[19] = '\\';keyMap[26] = '\'';keyMap[16] = ',';keyMap[24] = '.';keyMap[41] = '/';keyMap[43] = ' ';keyMap[3] = '\n';
+
+}
